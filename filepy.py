8f6a830ffc2a98d5852c34593251893625ec1dea
Anuj Goyal <anujg935@gmail.com>
2019-06-13 21:34:23 +0530

    added connnect line algorithm in pythonfilegenerator

--- filepy.py
+++ filepy.py
@@ -1,11 +1,12 @@
 from OMChem.Flowsheet import Flowsheet
 from OMChem.Mixer import Mixer
 from OMChem.MatStm import MatStm
-comp = ['Argon', 'Air', 'Hydrogeniodide']
+comp = ['Glycerol', 'Sulfurtrioxide', 'Nitricacid']
 Mixer1 = Mixer(name='Mixer1',NOI=2)
 MatStm1 = MatStm(name='MatStm1',T=300,P=101325,MolFlow=100,CompMolFrac=[0.5,0.4,0.1])
 MatStm2 = MatStm(name='MatStm2',T=300,P=101325,MolFlow=100,CompMolFrac=[0.5,0.4,0.1])
 MatStm3 = MatStm(name='MatStm3',T=300,P=101325,MolFlow=100,CompMolFrac=[0.5,0.4,0.1])
+Mixer1.connect(InputStms=['MatStm3', 'MatStm1'],OutputStms=MatStm2)
 f = Flowsheet()
 f.add_comp_list(comp)
 f.add_UnitOpn(Mixer1)
--- pythonGenerator.py
+++ pythonGenerator.py
@@ -21,6 +21,27 @@ def PythonFileGenerator(data):
             pass
 
 
+    templist = []
+    for i in range(len(data.index)):
+        if((data.iloc[i][1:6].isna().sum() <5) and (data.iloc[i][6:11].isna().sum() <5)):
+            l = pd.notna(data.iloc[i][1:6]).index[pd.notna(data.iloc[i][1:6]) == True].tolist()
+            print(l)
+            if(len(l)==1):
+                f.write(str(data.index.values[i])+".connect(InputStms="+str(data.iloc[i][l[0]])+",OutputStms=")
+            else:
+                t = []
+                t.append([data.iloc[i][l[j]] for j in range(len(l))])
+                f.write(str(data.index.values[i])+".connect(InputStms="+str(t[0])+",OutputStms=")
+            
+            li = pd.notna(data.iloc[i][6:11]).index[pd.notna(data.iloc[i][6:11]) == True].tolist()
+            print(li)
+            if(len(li)==1):
+                f.write(str(data.iloc[i][li[0]])+")\n")
+            else:
+                t = []
+                t.append([data.iloc[i][li[j]] for j in range(len(li))])
+                f.write(str(t[0])+")\n")
+
 
     f.write("f = Flowsheet()\n")
     f.write("f.add_comp_list(comp)\n")
